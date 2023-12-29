929f3164d3648be95830408c42c73d49c4a04ba7
shyfty-eyed <nifect@gmail.com>
2023-12-29 11:03:04 -0500

    day 4 working simple recorder

--- src/main/java/org/homevision/service/Config.java
+++ src/main/java/org/homevision/service/Config.java
@@ -3,15 +3,15 @@ package org.homevision.service;
 import com.google.gson.Gson;
 import lombok.Data;
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.nio.charset.Charset;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 @Component
 public class Config {
@@ -20,13 +20,13 @@ public class Config {
     public static class VideoSettings {
 
         private String name;
-        private int deviceId;
-        private int frameWidth;
-        private int frameHeight;
-        private int fps;
-        private int fileIntervalSeconds;
+        private Integer deviceId;
+        private Integer frameWidth;
+        private Integer frameHeight;
+        private Integer fps;
+        private Integer fileIntervalSeconds;
         private String videoFormat;
-        private int videoQuality;
+        private Integer videoQuality;
         private String videoOutPath;
     }
 
@@ -36,34 +36,25 @@ public class Config {
         private List<VideoSettings> captureDevices;
     }
 
-    private ConfigDto rawData;
-
-    private Map<String, VideoSettings> configHolder;
-
-    public VideoSettings get(String name) {
-        return configHolder.get(name);
-    }
+    private ConfigDto data;
 
     public List<VideoSettings> getAll() {
-        return rawData.captureDevices;
+        return data.captureDevices;
     }
 
 
     @PostConstruct
-    void load() throws IOException, IllegalAccessException {
-        Gson gson = new Gson();
+    private void load() throws IOException, IllegalAccessException {
         String json = IOUtils.resourceToString("/conf/config.json", Charset.defaultCharset());
-        rawData = gson.fromJson(json, ConfigDto.class);
+        data = (new Gson()).fromJson(json, ConfigDto.class);
 
-        configHolder = new HashMap<>(rawData.captureDevices.size());
 
-        for (var deviceConfig : rawData.captureDevices) {
-            for (Field f : deviceConfig.getClass().getFields()) {
-                if (f.get(deviceConfig) == null) {
-                    f.set(deviceConfig, f.get(rawData.global));
-                }
-            }
-            configHolder.put(deviceConfig.getName(), deviceConfig);
+        for (var deviceConfig : data.captureDevices) {
+            var dst = new BeanWrapperImpl(deviceConfig);
+            var definedProps = Arrays.stream(dst.getPropertyDescriptors())
+                    .map(pd -> pd.getName())
+                    .filter(name -> dst.getPropertyValue(name) != null).toArray(String[]::new);
+            BeanUtils.copyProperties(data.global, deviceConfig, definedProps);
         }
     }
 
